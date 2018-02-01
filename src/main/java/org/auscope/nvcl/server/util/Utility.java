76f984ff47cfdc6dbe601ad9d4594e874f4f8005
PeterWarren <peter.warren@csiro.au>
2018-02-01 12:27:25 +1100

    changed gethost utility function to be able to handle urls with more than 1 path level
    changed gethost utility function to be able to handle urls with more than 1 path level

--- src/main/java/org/auscope/nvcl/server/util/Utility.java
+++ src/main/java/org/auscope/nvcl/server/util/Utility.java
@@ -165,19 +165,19 @@ public class Utility {
     public static String getHost(String url) {
         if (url == null || url.length() == 0)
             return "";
-        int doubleslash = url.indexOf("//");
-        if (doubleslash == -1)
-            doubleslash = 0;
-        else
-            doubleslash += 2;
+    //    int doubleslash = url.indexOf("//");
+    //    if (doubleslash == -1)
+    //        doubleslash = 0;
+     //   else
+     //       doubleslash += 2;
 
-        int end = url.indexOf('/', doubleslash);
-        end = end >= 0 ? end : url.length();
+    //    int end = url.indexOf('/', doubleslash);
+     //   end = end >= 0 ? end : url.length();
 
-        int port = url.indexOf(':', doubleslash);
-        end = (port > 0 && port < end) ? port : end;
+      //  int port = url.indexOf(':', doubleslash);
+     //   end = (port > 0 && port < end) ? port : end;
 
-        String host = url.substring(0, end) + '/';
+        String host = url.substring(0, url.lastIndexOf('/',url.lastIndexOf('/')-1);
         return host;
 
     }
